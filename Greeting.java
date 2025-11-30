f15e530d215c913c01e466a4242ec7f8715ca701
Aleksadv <4545666.89az@gmail.com>
2025-11-30 16:48:11 +0300

    practice 2

---
+++ Greeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Greeting{
+
+    public static void  main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+    }
+    }
